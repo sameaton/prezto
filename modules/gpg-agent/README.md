5a22c6545c88e8f9da1066a1b2a0b97f6e29efa9
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-04-27 17:59:16 -0400

    Rewrite the gpg-agent module

--- modules/gpg-agent/README.md
+++ modules/gpg-agent/README.md
@@ -17,7 +17,6 @@ Authors
 
 *The authors of this module should be contacted via the [issue tracker][2].*
 
-  - [Florian Walch](https://github.com/fwalch)
   - [Sorin Ionescu](https://github.com/sorin-ionescu)
 
 [1]: http://linux.die.net/man/1/gpg-agent
--- modules/gpg-agent/init.zsh
+++ modules/gpg-agent/init.zsh
@@ -2,9 +2,7 @@
 # Provides for an easier use of gpg-agent.
 #
 # Authors:
-#   Florian Walch <florian.walch@gmx.at>
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
-#   neersighted <neersighted@myopera.com>
 #
 
 # Return if requirements are not found.
@@ -12,35 +10,27 @@ if (( ! $+commands[gpg-agent] )); then
   return 1
 fi
 
+# Get the SSH support option.
+zstyle -b ':prezto:module:gpg-agent' ssh-support '_gpg_agent_ssh_support' \
+  || _gpg_agent_ssh_support=''
+
+# Set the default path to the gpg-agent-info file.
 _gpg_agent_info="$HOME/.gpg-agent-info"
 
-function _gpg-agent-start {
-  local ssh_support
+# Start gpg-agent.
+ps -U "$USER" -o 'command' | egrep -q '^gpg-agent( |$)' \
+  || gpg-agent \
+      --daemon \
+      ${_gpg_agent_ssh_support:+'--enable-ssh-support'} \
+      --write-env-file "$_gpg_agent_info" > /dev/null
 
-  zstyle -b ':prezto:module:gpg-agent' ssh-support 'ssh_support' \
-    || ssh_support=''
-
-  gpg-agent \
-    --daemon \
-    ${ssh_support:+'--enable-ssh-support'} \
-    --write-env-file "${_gpg_agent_info}" > /dev/null
-
-  chmod 600 "${_gpg_agent_info}"
-  source "${_gpg_agent_info}" > /dev/null
-}
-
-# Source GPG agent settings, if applicable.
-if [[ -s "${_gpg_agent_info}" ]]; then
-  source "${_gpg_agent_info}" > /dev/null
-  ps -U "$USER" -o 'command,pid' \
-    | grep "${${(@s.:.)GPG_AGENT_INFO}[2]}" \
-    | grep -q '^gpg-agent' \
-  || _gpg-agent-start
-else
-  _gpg-agent-start
-fi
+chmod 600 "${_gpg_agent_info}" 2> /dev/null
+source "${_gpg_agent_info}" > /dev/null
 
 export GPG_AGENT_INFO
+export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK
 export SSH_AGENT_PID
-export GPG_TTY="$(tty)"
+
+unset _gpg_agent_{info,ssh_support}
+
